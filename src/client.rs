c72101d21f07a4ce6ba3489bee279431508e95fd
t9a <t9a.dev@icloud.com>
2025-07-01 13:44:32 +0900

    cargo fmt

--- src/client.rs
+++ src/client.rs
@@ -6,11 +6,11 @@ use crate::api::auth::RadikoAuthManager;
 
 #[derive(Debug, Clone)]
 pub struct RadikoClient {
-    inner: Arc<RadikoClientRef>
+    inner: Arc<RadikoClientRef>,
 }
 
 #[derive(Debug, Clone)]
-struct RadikoClientRef{
+struct RadikoClientRef {
     auth_manager: RadikoAuthManager,
     http_client: Client,
     area_id: String,
@@ -19,17 +19,15 @@ struct RadikoClientRef{
 impl RadikoClient {
     pub async fn new(radiko_auth_manager: RadikoAuthManager) -> Self {
         Self {
-            inner: Arc::new(
-                RadikoClientRef { 
+            inner: Arc::new(RadikoClientRef {
                 auth_manager: radiko_auth_manager.clone(),
                 http_client: radiko_auth_manager.get_http_client(),
                 area_id: radiko_auth_manager.get_area_id().await.unwrap(),
-            }
-            )
+            }),
         }
     }
 
-    pub fn get_auth_manager(&self) -> RadikoAuthManager{
+    pub fn get_auth_manager(&self) -> RadikoAuthManager {
         self.inner.auth_manager.clone()
     }
 
